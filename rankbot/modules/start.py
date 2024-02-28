a2c1986620b8ebf25ec1932debdbf8a2d325857c
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-29 00:47:43 +0530

    Update start.py

--- rankbot/modules/start.py
+++ rankbot/modules/start.py
@@ -7,7 +7,7 @@ import asyncio
 # --------------------------------------------------------------------------
 
 start_txt = """
-**🤖 *𝖱𝖠𝖭𝖪𝖨𝖭𝖦 𝖡𝖮𝖳*
+**🤖 *SANKIXD 𝖱𝖠𝖭𝖪𝖨𝖭𝖦 𝖡𝖮𝖳*
 
 𝖧𝖤𝖫𝖫𝖮  \n\n👋 𝖨'𝖬 𝖸𝖮𝖴𝖱 𝖥𝖱𝖨𝖤𝖭𝖣𝖫𝖸 𝖱𝖠𝖭𝖪𝖨𝖭𝖦 𝖡𝖮𝖳. \n
 🚀 𝖢𝖴𝖱𝖨𝖮𝖴𝖲 𝖠𝖡𝖮𝖴𝖳 𝖳𝖧𝖤 𝖬𝖮𝖲𝖳 𝖠𝖢𝖩𝖨𝖵𝖤 𝖬𝖤𝖬𝖡𝖤𝖱𝖲?
@@ -18,7 +18,7 @@ start_txt = """
 @app.on_message(filters.command("start")& filters.private)
 async def start(_, message):
     start_txt = (
-        "🤖 **𝖱𝖠𝖭𝖪𝖨𝖭𝖦 𝖡𝖮𝖳**\n\n"
+        "🤖 **SANKIXD 𝖱𝖠𝖭𝖪𝖨𝖭𝖦 𝖡𝖮𝖳**\n\n"
         "**𝖧𝖤𝖫𝖫𝖮**  \n\n👋 **𝖨'𝖬 𝖸𝖮𝖴𝖱 𝖥𝖱𝖨𝖤𝖭𝖣𝖫𝖸 𝖱𝖠𝖭𝖪𝖨𝖭𝖦 𝖡𝖮𝖳.**\n"
         "🚀 **𝖢𝖴𝖱𝖨𝖮𝖴𝖲 𝖠𝖡𝖮𝖴𝖳 𝖳𝖧𝖤 𝖬𝖮𝖲𝖳 𝖠𝖢𝖩𝖨𝖵𝖤 𝖬𝖤𝖬𝖡𝖤𝖱𝖲?**\n"
         "**𝖲𝖳𝖠𝖸 𝖳𝖴𝖭𝖤𝖣 𝖥𝖮𝖱 𝖴𝖯𝖣𝖠𝖳𝖤𝖲 𝖮𝖧 𝖬𝖤𝖲𝖲𝖠𝖦𝖨𝖭𝖦 𝖠𝖢𝖧𝖨𝖤𝖵𝖤𝖬𝖤𝖭𝖳𝖲!**\n"
@@ -30,15 +30,15 @@ async def start(_, message):
             InlineKeyboardButton("➕𝖠𝖣𝖣 𝖬𝖤 𝖨𝖭 𝖸𝖮𝖴𝖱 𝖦𝖱𝖮𝖴𝖯➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-            InlineKeyboardButton("🆘𝖲𝖴𝖯𝖱𝖮𝖳🆘", url="https://t.me/DAXXSUPPORT"),
-            InlineKeyboardButton("🧑‍💻𝖣𝖤𝖵🧑‍💻", user_id=OWNER_ID)
+            InlineKeyboardButton("๏ sᴜᴘᴘᴏʀᴛ ๏", url="https://t.me/sankibotkiduniya"),
+            InlineKeyboardButton("๏ ᴄʀᴇᴀᴛᴏʀ ๏", user_id=OWNER_ID)
         ]
     ]
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await message.reply_photo(
-        photo="https://graph.org/file/f77b368fdbc1431973c36.jpg",
+        photo="https://telegra.ph/file/411e736bdfece0511bda2.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
